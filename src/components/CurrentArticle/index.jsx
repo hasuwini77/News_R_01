5178f9113c7f617d90c228d335742006d07b83f4
Edwin Andermyr <andermyredwin@gmail.com>
2024-03-22 17:00:59 +0100

    trying to figure out the exit

--- src/components/CurrentArticle/index.jsx
+++ src/components/CurrentArticle/index.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import styles from './CurrentArticle.module.css';
 
 const CurrentArticle = ({ article }) => {
+    const handleClick = () => {
+        article(null)
+    }
+
     return (
         <div className={styles.article}>
             {article.headline}
+            <button onClick={handleClick} className={styles.button}> Go back</button>
         </div>
     );
 }
